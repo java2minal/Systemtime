03e1c273f17245835f168223290fc11102a609b4
java2minal <minal_saeed@hotmail.com>
2015-05-29 18:28:27 +0800

    Initial commit

---
+++ app/src/main/java/com/luct/minal/systemtime/MainActivity.java
@@ -0,0 +1,75 @@
+package com.luct.minal.systemtime;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.text.format.Time;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Calendar c = Calendar.getInstance();
+
+        System.out.println("Current time => "+c.getTime());
+
+
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        String formattedDate = df.format(c.getTime());
+
+
+        // formattedDate have current date/time
+
+        Toast.makeText(this, formattedDate, Toast.LENGTH_SHORT).show();
+
+
+
+        // Now we display formattedDate value in TextView
+
+        TextView txtView = new TextView(this);
+
+        txtView.setText("Current Date and Time : "+formattedDate);
+
+        txtView.setGravity(Gravity.CENTER);
+
+        txtView.setTextSize(20);
+
+        setContentView(txtView);
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
